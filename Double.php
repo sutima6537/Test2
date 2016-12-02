4d7ad3282d5be3fa3df26b8aa2bc3afaed14606a
sutima6537 <sutima6537@gmail.com>
2016-12-02 19:51:02 +0700

    new

---
+++ Double.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>EZ booking</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="bulma/css/bulma.css">
+  <link rel="stylesheet" href="bulma/css/bulma.min.css.map">
+  <link rel="stylesheet" href="bulma/bulma.saas">
+  <link rel="stylesheet" href="css/custom.css">
+  <link href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oswald|Trirong&effect=shadow-multiple">
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  
+  <link rel="stylesheet" href="js/fancybox/source/jquery.fancybox.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="js/fancybox/source/helpers/jquery.fancybox-buttons.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="js/fancybox/source/helpers/jquery.fancybox-buttons.js"></script>
+  <script type="text/javascript" src="js/fancybox/source/helpers/jquery.fancybox-media.js"></script>
+  <link rel="stylesheet" href="js/fancybox/source/helpers/jquery.fancybox-thumbs.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="js/fancybox/source/helpers/jquery.fancybox-thumbs.js"></script>
+  <script type="text/javascript" src="js/fancybox/source/jquery.fancybox.pack.js"></script>
+  <script type="text/javascript" src="js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+  
+
+
+
+<script type="text/javascript">
+  $(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+});
+  </script>
+  </head>
+
+<body style="padding-top: 25px;">
+
+    <!--nav-->
+  <nav class="nav shadow fixed-nav-bar">
+    <div class="container section-max-width">
+      <div class="nav-left">
+        <a class="nav-item" href="#">
+          <span class="icon" style="color:#3B5998">
+        <i class="fa fa-facebook-square" ></i>
+      </span>
+        </a>
+      </div>
+      <div class="nav-center">
+        <a class="nav-item is-brand font-effect-shadow-multiple" href="#">
+          <h1 style="font-head">
+            <font size="5" color="#329999">EZ booking</font>
+          </h1>
+        </a>
+      </div>
+      <span class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+      </span>
+      <div class="nav-right nav-menu">
+        <span class="nav-item">
+      <a class="button is-danger is-outlined" >
+        <span>Register</span>
+        </a>
+        <a class="button is-success" href="#">
+          <span class="icon">
+          <i class="fa fa-sign-in"></i>
+        </span>
+          <span>Login</span>
+        </a>
+        </span>
+      </div>
+    </div>
+  </nav>
+  <!--end nav-->
+<section class="hero is-fullheight is-dark is-bold">
+
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column">
+
+            <div class="box notification is-light">
+<div class="tile is-ancestor">
+  <div class="tile is-vertical is-8">
+    <div class="tile">
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-primary">
+          <p class="title" >S19 @ Hotel</p>
+          <p class="subtitle">Twin  Room</p>
+          <figure class="image is-4by3">
+            <img src="room5/7.jpg">
+          </figure>
+        </article>
+      </div>
+    </div>
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-warning">
+        <center>
+        <div class="content">
+
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm6.staticflickr.com/5444/17679973232_568353a624_b.jpg" >
+	<img src="http://farm6.staticflickr.com/5444/17679973232_568353a624_m.jpg " alt="" style="width:150px; height:100px;" />
+</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm8.staticflickr.com/7367/16426879675_e32ac817a8_b.jpg" >
+	<img src="http://farm8.staticflickr.com/7367/16426879675_e32ac817a8_m.jpg" alt="" style="width:150px; height:100px;" />
+</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm6.staticflickr.com/5612/15344856989_449794889d_b.jpg" >
+	<img src="http://farm6.staticflickr.com/5612/15344856989_449794889d_m.jpg" alt="" style="width:150px; height:100px;" />
+</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm8.staticflickr.com/7289/16207238089_0124105172_b.jpg">
+	<img src="http://farm8.staticflickr.com/7289/16207238089_0124105172_m.jpg" alt="" style="width:150px; height:100px;" />
+</a>
+
+        </div>
+        </center>
+      </article>
+    </div>
+  </div>
+  <div class="tile is-parent">
+    <article class="tile is-child notification is-dark">
+      <div class="content">
+        <p class="title">รายละเอียด</p>
+          <ul>
+                    <li>ย่าน : พัทยา, ชลบุรี</li>
+                    <li>จำนวนผู้เข้าพัก : ไม่เกิน 3 คน (คนที่ 3 ต้องเช่าเตียงเสริมเพิ่ม)</li>
+                    <li>ห้อง : 1 x Single Bedded Room(Single โอโซน)</li>
+                    <li>รายละเอียดห้อง : 1 ห้องนอน, 1 ห้องน้ำ</li>
+                </ul>
+
+          <br>
+        <p class="title">สิ่งอำนวยความสะดวก</p>
+          <ul>
+                    <li>ฟรี Wi-Fi</li>
+                    <li>เครื่องทำน้ำอุ่น</li>
+                    <li>TV, ตู้เย็น, ไมโครเวฟ</li>
+                    <li>สระว่ายน้ำ(กลางแจ้ง)</li>
+                    <br>
+                </ul>
+      </div>
+
+    <nav class="level">
+      <div class="level-right">
+
+      </div>
+
+      <div class="level-right">
+          <div class="level-item">
+          <a class="button is-success is-inverted is-large">
+            <span class="icon">
+              <i class="fa fa-shopping-cart"></i>
+            </span>
+            <span>จอง</span>
+          </a>
+          </div>
+      </div>
+    </nav>
+      
+    </article>
+  </div>
+  
+</div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+
+</body>
+
+</html>
